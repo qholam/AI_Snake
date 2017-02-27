da6179b638cef9633b5fb4d2125f62c4adf4da8a
qholam <qholam@wpi.edu>
2017-02-27 16:07:37 -0500

    initial commit
    initial commit

---
+++ smart-snake/src/main/java/guojian/smart_snake/Config.java
@@ -0,0 +1,18 @@
+
+package guojian.smart_snake;
+
+/**
+ * Configuration
+ * @author guojian
+ * @date 2016年12月30日 下午6:04:37
+ * @email 1181819395@qq.com
+ */
+public class Config {
+
+	public static final int MAZE_ROWS = 40;//Must larger then 9, otherwise the snake is in the wall
+	public static final int MAZE_COLS = 40;
+	public static final double SPEED = 10; //Speed
+	public static final boolean SAVE = false; //Default as not save
+	public static final String DIR = "snakeGame";//save to file "snakeGame"
+
+}
